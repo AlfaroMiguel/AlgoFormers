460ceedf529c9e5af33a432cfbbc727af335e863
AlfaroMiguel <miguelalfaro95@hotmail.com>
2016-05-28 20:55:13 -0300

    Arreglo paquetes

--- src/fiuba/algo3/algoformers/modos/Modo.java
+++ src/fiuba/algo3/algoformers/modos/Modo.java
@@ -3,14 +3,10 @@ package fiuba.algo3.algoFormers.modos;
 import fiuba.algo3.algoFormers.autobots.Autobot;
 import fiuba.algo3.algoFormers.decepticons.Decepticon;
 import fiuba.algo3.algoFormers.generico.Algoformer;
-<<<<<<< HEAD
-fiuba.algo3.algoFormers.Algoformer;
-=======
->>>>>>> 50141a8f524a316f866abc74cc649178add4c3e7
 
 public interface Modo{
 	
 	public void atacar(Autobot atacante, Algoformer atacado);
 	public void atacar(Decepticon atacante, Algoformer atacado);
-	public void cambiar(algoformer);
+	public void cambiar(Algoformer algoformer);
 }
