039736376981d10109aa787f828d13f190bb7b05
Agustina Mendez <abmendez25@hotmail.com>
2016-05-28 17:47:15 -0300

    Update algoformersTest.java

--- test/fiuba/algo3/algoFormers/algoformersTest.java
+++ test/fiuba/algo3/algoFormers/algoformersTest.java
@@ -2,16 +2,11 @@ package fiuba.algo3.algoFormers;
 
 import static org.junit.Assert.*;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-//import fiuba.algo3.algoformers.Alterno;
-//import fiuba.algo3.algoformers.Modo;
-import fiuba.algo3.*;
-import fiuba.algo3.algoformers.autobots.Autobot;
 import fiuba.algo3.algoformers.autobots.Optimus;
-import fiuba.algo3.algoformers.decepticons.Decepticon;
-import fiuba.algo3.algoformers.modos.OptimusHumanoide;
+import fiuba.algo3.algoformers.modos.OptimusAlterno;
+
 
 public class algoformersTest {
 
@@ -19,8 +14,7 @@ public class algoformersTest {
 	public void test01CreoUnTransformerYcambioSuModo(){
 		Optimus optimus = new Optimus();
 		optimus.cambiarModo();
-		Assert.assertTrue(optimus.verModo()==OptimusHumanoide.getInstance());
-		
+		assertEquals(optimus.verModo(),OptimusAlterno.getInstance());	
 	}
 	
 
