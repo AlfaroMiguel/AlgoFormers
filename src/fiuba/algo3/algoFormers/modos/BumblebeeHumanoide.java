d747eb76ece1849f3b3ea57dd08f98567ef99bb6
Agustina <abmendez25@hotmail.com>
2016-05-28 20:47:20 -0300

    cambie los nombres de algunos paquetes

---
+++ src/fiuba/algo3/algoFormers/modos/BumblebeeHumanoide.java
@@ -0,0 +1,42 @@
+package fiuba.algo3.algoFormers.modos;
+
+import fiuba.algo3.algoFormers.autobots.Autobot;
+import fiuba.algo3.algoFormers.decepticons.Decepticon;
+import fiuba.algo3.algoFormers.generico.Algoformer;
+
+public class BumblebeeHumanoide implements Modo {
+	
+	private static BumblebeeHumanoide instancia;
+	private int distAtaque;
+	private int ataque;
+	private int velocidad;
+	
+	private BumblebeeHumanoide(){
+		this.ataque = 40;
+		this.distAtaque = 1;
+		this.velocidad = 2;
+	}
+	
+	public static BumblebeeHumanoide getInstance(){
+		if (instancia == null){
+			instancia = new BumblebeeHumanoide();
+		}
+		return instancia;
+	}
+	
+	@Override
+	public void cambiar(Algoformer algoformer) {
+		algoformer.establecerModo(BumblebeeAlterno.getInstance());
+	}
+
+	@Override
+	public void atacar(Autobot atacante, Algoformer atacado) {
+		atacado.serAtacado(atacante, this.ataque);	
+	}
+
+	@Override
+	public void atacar(Decepticon atacante, Algoformer atacado) {
+		atacado.serAtacado(atacante, this.ataque);
+	}
+	
+}
