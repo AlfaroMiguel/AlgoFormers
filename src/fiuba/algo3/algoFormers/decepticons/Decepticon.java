f887fe692928b14b59cc8bee7da9d7abaddb052b
Agustina Mendez <abmendez25@hotmail.com>
2016-05-29 19:20:41 -0300

    anda seleccionar algoformer recibiendo coordenadas

--- src/fiuba/algo3/algoFormers/decepticons/Decepticon.java
+++ src/fiuba/algo3/algoFormers/decepticons/Decepticon.java
@@ -1,7 +1,10 @@
 package fiuba.algo3.algoFormers.decepticons;
 
 import fiuba.algo3.algoFormers.Habitables.HabitableDelMapa;
+import fiuba.algo3.algoFormers.Juego.Autobots;
+import fiuba.algo3.algoFormers.Juego.Decepticons;
 import fiuba.algo3.algoFormers.autobots.Autobot;
+import fiuba.algo3.algoFormers.excepciones.EquipoInvalidoException;
 import fiuba.algo3.algoFormers.excepciones.MismoEquipoException;
 import fiuba.algo3.algoFormers.generico.Algoformer;
 
@@ -23,6 +26,16 @@ public class Decepticon extends Algoformer {
 	@Override
 	public void atacar(HabitableDelMapa atacado) {
 		this.modo.atacar(this,atacado);		
+	}
+	
+	@Override
+	public void serSeleccionado(Autobots equipoAutobot) {
+		throw new EquipoInvalidoException();
+	}
+
+	@Override
+	public void serSeleccionado(Decepticons equipoDecepticon) {
+		equipoDecepticon.setAlgoformerActual(this);
 	}
 
 }
