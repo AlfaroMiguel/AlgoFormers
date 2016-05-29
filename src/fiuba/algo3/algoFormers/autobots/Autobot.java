c8648e718d9421313ce6094e525af091d693de13
Agustina Mendez <abmendez25@hotmail.com>
2016-05-29 19:19:43 -0300

    anda seleccionar algoformer recibiendo coordenadas

--- src/fiuba/algo3/algoFormers/autobots/Autobot.java
+++ src/fiuba/algo3/algoFormers/autobots/Autobot.java
@@ -1,7 +1,10 @@
 package fiuba.algo3.algoFormers.autobots;
 
 import fiuba.algo3.algoFormers.Habitables.HabitableDelMapa;
+import fiuba.algo3.algoFormers.Juego.Autobots;
+import fiuba.algo3.algoFormers.Juego.Decepticons;
 import fiuba.algo3.algoFormers.decepticons.Decepticon;
+import fiuba.algo3.algoFormers.excepciones.EquipoInvalidoException;
 import fiuba.algo3.algoFormers.excepciones.MismoEquipoException;
 import fiuba.algo3.algoFormers.generico.Algoformer;
 
@@ -26,4 +29,13 @@ public class Autobot extends Algoformer{
 		this.modo.atacar(this,atacado);	
 	}
 	
+	@Override
+	public void serSeleccionado(Autobots equipoAutobot) {
+		equipoAutobot.setAlgoformerActual(this);
+	}
+
+	@Override
+	public void serSeleccionado(Decepticons equipoDecepticon) {
+		throw new EquipoInvalidoException();
+	}
 }
