413b38769a4bd930cfbf10bbc0af4958e1991d06
Agustina Mendez <abmendez25@hotmail.com>
2016-05-29 19:22:09 -0300

    anda seleccionar algoformer recibiendo coordenadas

--- test/fiuba/algo3/algoFormers/JuegoTests/JugadorTest.java
+++ test/fiuba/algo3/algoFormers/JuegoTests/JugadorTest.java
@@ -3,6 +3,7 @@ package fiuba.algo3.algoFormers.JuegoTests;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import fiuba.algo3.algoFormers.Habitables.Vacio;
 import fiuba.algo3.algoFormers.Juego.Autobots;
 import fiuba.algo3.algoFormers.Juego.Decepticons;
 import fiuba.algo3.algoFormers.Juego.Equipo;
@@ -12,6 +13,7 @@ import fiuba.algo3.algoFormers.Tablero.Tablero;
 import fiuba.algo3.algoFormers.autobots.*;
 import fiuba.algo3.algoFormers.decepticons.*;
 import fiuba.algo3.algoFormers.excepciones.EquipoInvalidoException;
+import fiuba.algo3.algoFormers.excepciones.NoAlgoformerException;
 
 
 public class JugadorTest {
@@ -22,7 +24,9 @@ public class JugadorTest {
 		Tablero tablero = new Tablero(10,10);
 		Jugador jugador = new Jugador(autobots, tablero);
 		Optimus optimus = new Optimus();
-		jugador.seleccionarAlgoformer(optimus);
+		Coordenada coordenada = new Coordenada(2,4);
+		tablero.put(optimus, coordenada);
+		jugador.seleccionarAlgoformer(coordenada);
 		assertEquals(jugador.verAlgoformerActual(), optimus);
 	}
 	
@@ -32,7 +36,9 @@ public class JugadorTest {
 		Tablero tablero = new Tablero(10,10);
 		Jugador jugador = new Jugador(autobots, tablero);
 		Megatron megatron = new Megatron();
-		jugador.seleccionarAlgoformer(megatron);
+		Coordenada coordenada = new Coordenada(2,4);
+		tablero.put(megatron, coordenada);
+		jugador.seleccionarAlgoformer(coordenada);
 	}
 	
 	@Test (expected = EquipoInvalidoException.class)
@@ -41,7 +47,9 @@ public class JugadorTest {
 		Tablero tablero = new Tablero(10,10);
 		Jugador jugador = new Jugador(decepticons, tablero);
 		Optimus optimus = new Optimus();
-		jugador.seleccionarAlgoformer(optimus);
+		Coordenada coordenada = new Coordenada(2,4);
+		tablero.put(optimus, coordenada);
+		jugador.seleccionarAlgoformer(coordenada);
 	}
 	
 	@Test
@@ -50,18 +58,22 @@ public class JugadorTest {
 		Tablero tablero = new Tablero(10,10);
 		Jugador jugador = new Jugador(decepticons, tablero);
 		Megatron megatron = new Megatron();
-		jugador.seleccionarAlgoformer(megatron);
+		Coordenada coordenada = new Coordenada(2,4);
+		tablero.put(megatron, coordenada);
+		jugador.seleccionarAlgoformer(coordenada);
 		assertEquals(jugador.verAlgoformerActual(), megatron);
 	}
-//	@Test (expected = NoAlgoformerException.class)
-//	public void test05SeleccionarEspacioVacioEquipoAutobots(){
-//		Decepticons decepticons = new Decepticons();
-//		Tablero tablero = new Tablero(10,10);
-//		Jugador jugador = new Jugador(decepticons, tablero);
-//		Vacio espacioVacio = new Vacio();
-//		jugador.seleccionarAlgoformer(espacioVacio);
-//	}
-//	}
+
+	@Test (expected = NoAlgoformerException.class)
+	public void test05SeleccionarEspacioVacioEquipoAutobots(){
+		Decepticons decepticons = new Decepticons();
+		Tablero tablero = new Tablero(10,10);
+		Jugador jugador = new Jugador(decepticons, tablero);
+		Vacio espacioVacio = new Vacio();
+		Coordenada coordenada = new Coordenada(2,4);
+		tablero.put(espacioVacio, coordenada);
+		jugador.seleccionarAlgoformer(coordenada);
+	}
 	
 	@Test
 	public void test01UnJugadorAtacaConSuTransformerAOtroDeOtroEauipo(){
@@ -70,13 +82,12 @@ public class JugadorTest {
 	Jugador jugador = new Jugador(autobots, tablero);
 	Optimus optimus = new Optimus();
 	Frenzy frenzy = new Frenzy();
-	jugador.seleccionarAlgoformer(optimus);
-	
 	Coordenada coordenadaOptimus= new Coordenada(2,3);
 	Coordenada coordenadaFrenzy = new Coordenada(2,5);
 	tablero.put(optimus, coordenadaOptimus);
 	tablero.put(frenzy, coordenadaFrenzy);
-	
+	jugador.seleccionarAlgoformer(coordenadaOptimus);
+
 	jugador.atacar(coordenadaFrenzy);
 	assertEquals(frenzy.verVida(),350);
 	}
@@ -87,13 +98,12 @@ public class JugadorTest {
 	Tablero tablero = new Tablero (10,10);
 	Jugador jugador = new Jugador(autobots, tablero);
 	Optimus optimus = new Optimus();
-	jugador.seleccionarAlgoformer(optimus);
-	
 	Coordenada coordenada = new Coordenada(2,3);
 	Coordenada nuevaCoordenada= new Coordenada(2,1);
-	tablero.put(jugador.verAlgoformerActual(), coordenada); 
-	jugador.mover(nuevaCoordenada);
+	tablero.put(optimus, coordenada); 
+	jugador.seleccionarAlgoformer(coordenada);
 	
+	jugador.mover(nuevaCoordenada);
 	assertEquals(tablero.getKeyValue(jugador.verAlgoformerActual()), nuevaCoordenada);
 	}
 	
@@ -103,12 +113,12 @@ public class JugadorTest {
 	Tablero tablero = new Tablero (10,10);
 	Jugador jugador = new Jugador(equipoAutobots, tablero);
 	Optimus optimus = new Optimus();
-	jugador.seleccionarAlgoformer(optimus);
-	jugador.transformarAlgoformer();
-	
 	Coordenada coordenada = new Coordenada(2,3);
 	Coordenada nuevaCoordenada= new Coordenada(2,8);
-	tablero.put(jugador.verAlgoformerActual(), coordenada); 
+	tablero.put(optimus, coordenada);
+	jugador.seleccionarAlgoformer(coordenada);
+	jugador.transformarAlgoformer();
+	 
 	jugador.mover(nuevaCoordenada);
 	
 	assertEquals(tablero.getKeyValue(jugador.verAlgoformerActual()), nuevaCoordenada);
