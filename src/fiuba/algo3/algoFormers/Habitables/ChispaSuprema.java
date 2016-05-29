204e6a65ba8d6b419d3ab13fc1a8481e46901451
Agustina Mendez <abmendez25@hotmail.com>
2016-05-29 19:16:29 -0300

    anda seleccionar algoformer recibiendo coordenadas

--- src/fiuba/algo3/algoFormers/Habitables/ChispaSuprema.java
+++ src/fiuba/algo3/algoFormers/Habitables/ChispaSuprema.java
@@ -1,7 +1,10 @@
 package fiuba.algo3.algoFormers.Habitables;
 
+import fiuba.algo3.algoFormers.Juego.Autobots;
+import fiuba.algo3.algoFormers.Juego.Decepticons;
 import fiuba.algo3.algoFormers.autobots.Autobot;
 import fiuba.algo3.algoFormers.decepticons.Decepticon;
+import fiuba.algo3.algoFormers.excepciones.NoAlgoformerException;
 import fiuba.algo3.algoFormers.excepciones.NoAtacableException;
 
 public class ChispaSuprema implements Collectable{
@@ -27,5 +30,15 @@ public class ChispaSuprema implements Collectable{
 	@Override
 	public void serAtacado(Decepticon atacante, int ataque) {
 		throw new NoAtacableException();
+	}
+	
+	@Override
+	public void serSeleccionado(Autobots equipoAutobots) {
+		throw new NoAlgoformerException();
+	}
+
+	@Override
+	public void serSeleccionado(Decepticons equipoDecepticons) {
+		throw new NoAlgoformerException();
 	}
 }
