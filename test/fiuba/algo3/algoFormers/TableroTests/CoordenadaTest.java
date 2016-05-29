d8036828da976de990c3a174c0b3c2d8f94c9ded
guidoPeirano <guidopeirano11@hotmail.com>
2016-05-29 01:05:24 -0300

    Add files via upload

--- test/fiuba/algo3/algoFormers/TableroTests/CoordenadaTest.java
+++ test/fiuba/algo3/algoFormers/TableroTests/CoordenadaTest.java
@@ -1,8 +1,8 @@
-package fiuba.algo3.algoFormers.TableroTests;
+package fiuba.algo3.algoformers.tests;
 import org.junit.Assert;
 import org.junit.Test;
 
-import fiuba.algo3.algoFormers.Tablero.Coordenada;
+import fiuba.algo3.algoformers.Tablero.Coordenada;
 
 public class CoordenadaTest {
 
@@ -19,5 +19,12 @@ public class CoordenadaTest {
 		coordenada1.add(coordenada2);
 		Coordenada coordenada3 = new Coordenada(3,7);
 		Assert.assertEquals("Las coordenadas NO se comparan correctamente", coordenada1,coordenada3 );
+	}
+	@Test
+	public void test03LaDistanciaDeLasCoordenadasSeCalculanCorrectamente(){
+		Coordenada coordenada1 = new Coordenada(1,4);
+		Coordenada coordenada2 = new Coordenada(1,3);
+		double largo = 1;
+		Assert.assertTrue("La distancia entre las coordenadas NO se calcula correctamente",coordenada1.distancia(coordenada2)==largo);
 	}
 }
--- test/fiuba/algo3/algoFormers/TableroTests/TableroTests.java
+++ test/fiuba/algo3/algoFormers/TableroTests/TableroTests.java
@@ -1,28 +1,64 @@
-package fiuba.algo3.algoFormers.TableroTests;
+package fiuba.algo3.algoformers.tests;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import fiuba.algo3.algoFormers.Habitables.ChispaSuprema;
-import fiuba.algo3.algoFormers.Tablero.Coordenada;
-import fiuba.algo3.algoFormers.Tablero.Tablero;
+import fiuba.algo3.algoformers.Tablero.*;
+import fiuba.algo3.algoformers.Habitables.HabitableDelMapa;
+import fiuba.algo3.algoformers.Habitables.ChispaSuprema;
 
 public class TableroTests {
+	Tablero tablero = new Tablero(10,10);
+
 	@Test
 	public void test01ElTableroSeCreaVacio(){
-		Tablero tablero = new Tablero(10,10);
-		Assert.assertTrue("El tablero NO se crea vacio en todas las posicines", tablero.estaVacio(3,3));
-		Assert.assertTrue("El tablero NO se crea vacio en todas las posicines", tablero.estaVacio(2,5));
-		Assert.assertTrue("El tablero NO se crea vacio en todas las posicines", tablero.estaVacio(1,9));
+		Coordenada coordenada1 = new Coordenada(0,0);
+		Coordenada coordenada2 = new Coordenada(3,2);
+		Coordenada coordenada3 = new Coordenada(1,9);
+		Assert.assertTrue("El tablero NO se crea vacio en todas las posicines", tablero.estaVacio(coordenada1));
+		Assert.assertTrue("El tablero NO se crea vacio en todas las posicines", tablero.estaVacio(coordenada2));
+		Assert.assertTrue("El tablero NO se crea vacio en todas las posicines", tablero.estaVacio(coordenada3));
 	}
 
 	@Test
 	public void test02ElTableroUbicaHabitablesEnElMapa(){
-		Tablero tablero = new Tablero(10,10);
 		Coordenada coordenada = new Coordenada(3,2);
 		ChispaSuprema chispa = ChispaSuprema.getInstance();
 		tablero.put(chispa,coordenada);
-		Assert.assertTrue("El tablero NO agrega Habitables a posiciones", tablero.estaVacio(3,4));
+		Assert.assertTrue("El tablero NO agrega Habitables a posiciones", tablero.estaVacio(coordenada));
+	}
+	@Test
+	public void test03ElTableroEncuentraLasCoordenadasCorrectamente(){
+		Coordenada coordenada = new Coordenada(1,0);
+		ChispaSuprema chispa = ChispaSuprema.getInstance();
+		tablero.put(chispa,coordenada);
+		Assert.assertTrue("El tablero NO encuentra las coordenadas coorectamente", tablero.getKeyValue(chispa).equals(coordenada));
+	}
+
+	@Test (expected = ElementoNoExisteException.class)
+	public void test04ElTableroNoEncuentraElementosQueNoContiene(){
+		ChispaSuprema chispa = ChispaSuprema.getInstance();
+		tablero.getKeyValue(chispa);
+	}
+	@Test (expected = MovimientoInvalidoException.class)
+	public void test05ElTableroDesplazaUnHabitableDelMapa(){
+		Coordenada coordenadaInic = new Coordenada(3,4);
+		ChispaSuprema chispa = ChispaSuprema.getInstance();
+		tablero.put(chispa,coordenadaInic);
+		Coordenada coordenadaFinal = new Coordenada(3,7);
+		int paso = 2;
+		tablero.move(chispa,coordenadaFinal,paso);
+	}
+	@Test
+	public void test06ElTableroDesplazaUnHabitableDelMapa(){
+		Coordenada coordenadaInic = new Coordenada(3,4);
+		ChispaSuprema chispa = ChispaSuprema.getInstance();
+		tablero.put(chispa,coordenadaInic);
+		Coordenada coordenadaFinal = new Coordenada(3,7);
+		int paso = 3;
+		tablero.move(chispa,coordenadaFinal,paso);
+		Assert.assertTrue("El tablero NO vacia el espacio del cual se movio el habitable",tablero.estaVacio(coordenadaInic));
+		Assert.assertFalse("El tablero NO pone en el lugar de destino al habitable que se movio",tablero.estaVacio(coordenadaFinal));
 	}
 
 }
