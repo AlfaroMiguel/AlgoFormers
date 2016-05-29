894a03dd55a4d885022696598a3ca3c85498a791
Agustina Mendez <abmendez25@hotmail.com>
2016-05-29 19:17:13 -0300

    anda seleccionar algoformer recibiendo coordenadas

--- src/fiuba/algo3/algoFormers/Habitables/Vacio.java
+++ src/fiuba/algo3/algoFormers/Habitables/Vacio.java
@@ -1,8 +1,11 @@
 package fiuba.algo3.algoFormers.Habitables;
 
+import fiuba.algo3.algoFormers.Juego.Autobots;
+import fiuba.algo3.algoFormers.Juego.Decepticons;
 import fiuba.algo3.algoFormers.autobots.Autobot;
 import fiuba.algo3.algoFormers.decepticons.Decepticon;
-import fiuba.algo3.algoFormers.excepciones.NoAtacableException;
+import fiuba.algo3.algoFormers.excepciones.NoAlgoformerException;
+import fiuba.algo3.algoFormers.excepciones.NoAtacableException;;
 
 public class Vacio implements HabitableDelMapa{
 
@@ -23,5 +26,15 @@ public class Vacio implements HabitableDelMapa{
 	@Override
 	public void serAtacado(Decepticon atacante, int ataque) {
 		throw new NoAtacableException();
+	}
+	
+	@Override
+	public void serSeleccionado(Autobots equipoAutobots) {
+		throw new NoAlgoformerException();
+	}
+
+	@Override
+	public void serSeleccionado(Decepticons equipoDecepticons) {
+		throw new NoAlgoformerException();
 	}
 }
