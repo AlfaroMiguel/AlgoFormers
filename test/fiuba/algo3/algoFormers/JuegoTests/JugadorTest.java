9ff15732ba3a7f3ae5467623cadda6076e834226
Agustina <abmendez25@hotmail.com>
2016-05-29 15:53:56 -0300

    mas pruebas para jugador

--- test/fiuba/algo3/algoFormers/JuegoTests/JugadorTest.java
+++ test/fiuba/algo3/algoFormers/JuegoTests/JugadorTest.java
@@ -4,23 +4,55 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import fiuba.algo3.algoFormers.Juego.Autobots;
+import fiuba.algo3.algoFormers.Juego.Decepticons;
 import fiuba.algo3.algoFormers.Juego.Equipo;
 import fiuba.algo3.algoFormers.Juego.Jugador;
 import fiuba.algo3.algoFormers.Tablero.Coordenada;
 import fiuba.algo3.algoFormers.Tablero.Tablero;
 import fiuba.algo3.algoFormers.autobots.*;
 import fiuba.algo3.algoFormers.decepticons.*;
-
+import fiuba.algo3.algoFormers.excepciones.EquipoInvalidoException;
 
 
 public class JugadorTest {
 	
-//	@Test
-//	public void test01SeleccionarAutobotEquipoAutobots(){
-//		Autobots autobots = new Autobots();
-//		Tablero tablero = new Tablero(10,10);
-//		Jugador jugador = new Jugador(autobots, tablero);
-//		
+	@Test
+	public void test01SeleccionarAutobotEquipoAutobots(){
+		Autobots autobots = new Autobots();
+		Tablero tablero = new Tablero(10,10);
+		Jugador jugador = new Jugador(autobots, tablero);
+		Optimus optimus = new Optimus();
+		jugador.seleccionarAlgoformer(optimus);
+		assertEquals(jugador.verAlgoformerActual(), optimus);
+	}
+	
+	@Test (expected = EquipoInvalidoException.class)
+	public void test02SeleccionarDecepticonEquipoAutobots(){
+		Autobots autobots = new Autobots();
+		Tablero tablero = new Tablero(10,10);
+		Jugador jugador = new Jugador(autobots, tablero);
+		Megatron megatron = new Megatron();
+		jugador.seleccionarAlgoformer(megatron);
+	}
+	
+	@Test (expected = EquipoInvalidoException.class)
+	public void test03SeleccionarAutobotEquipoDecepticons(){
+		Decepticons decepticons = new Decepticons();
+		Tablero tablero = new Tablero(10,10);
+		Jugador jugador = new Jugador(decepticons, tablero);
+		Optimus optimus = new Optimus();
+		jugador.seleccionarAlgoformer(optimus);
+	}
+	
+	@Test
+	public void test04SeleccionarDecepticonEquipoDecepticons(){
+		Decepticons decepticons = new Decepticons();
+		Tablero tablero = new Tablero(10,10);
+		Jugador jugador = new Jugador(decepticons, tablero);
+		Megatron megatron = new Megatron();
+		jugador.seleccionarAlgoformer(megatron);
+		assertEquals(jugador.verAlgoformerActual(), megatron);
+	}
 //	}
 //	@Test
 //	public void test01UnJugadorAtacaConSuTransformerAOtroDeOtroEauipo(){
