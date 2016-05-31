387bc34bd9e14468a3e56b2dce64e547e152666f
guidoPeirano <guidopeirano11@hotmail.com>
2016-05-31 11:43:13 -0300

    Este si es la posta

---
+++ src/fiuba/algo3/algoFormers/Juego/UbicadorDePersonajes.java
@@ -0,0 +1,28 @@
+package fiuba.algo3.algoFormers.Juego;
+
+import java.util.*;
+
+import fiuba.algo3.algoFormers.Tablero.Coordenada;
+import fiuba.algo3.algoFormers.Tablero.Tablero;
+import fiuba.algo3.algoFormers.autobots.Autobot;
+import fiuba.algo3.algoFormers.decepticons.Decepticon;
+
+
+public class UbicadorDePersonajes {
+
+	public static void posicionarEquipoAutobot(List<Autobot> integrantes, Tablero tablero) {
+		tablero.put(integrantes.get(0), new Coordenada(1,2));
+	 	tablero.put(integrantes.get(1), new Coordenada(2,2));
+	 	tablero.put(integrantes.get(2), new Coordenada(2,1));
+		
+	}
+
+	public static void posicionarEquipoDecepticon(List<Decepticon> integrantes, Tablero tablero) {
+		tablero.put(integrantes.get(0), new Coordenada(18,2));
+	 	tablero.put(integrantes.get(1), new Coordenada(18,2));
+	 	tablero.put(integrantes.get(2), new Coordenada(19,1));
+		
+	}
+
+
+}
