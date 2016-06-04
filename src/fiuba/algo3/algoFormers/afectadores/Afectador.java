78eb14fdd404cfb64a20102a12b1a4f846927c25
guidoPeirano <guidopeirano11@hotmail.com>
2016-06-04 15:21:52 -0300

    Creo que ahi lo solucione

--- src/fiuba/algo3/algoFormers/afectadores/Afectador.java
+++ src/fiuba/algo3/algoFormers/afectadores/Afectador.java
@@ -1,7 +1,6 @@
 package fiuba.algo3.algoFormers.afectadores;
 
 import fiuba.algo3.algoFormers.efectos.Efecto;
-import fiuba.algo3.algoFormers.efectosTest.*;
 import fiuba.algo3.algoFormers.generico.*;
 
 public abstract class Afectador{
--- src/fiuba/algo3/algoFormers/afectadores/AfectadorPorPosicion.java
+++ src/fiuba/algo3/algoFormers/afectadores/AfectadorPorPosicion.java
@@ -2,7 +2,6 @@ package fiuba.algo3.algoFormers.afectadores;
 
 import fiuba.algo3.algoFormers.efectos.Efecto;
 import fiuba.algo3.algoFormers.efectos.EfectoTerminado;
-import fiuba.algo3.algoFormers.efectosTest.*;
 import fiuba.algo3.algoFormers.generico.Algoformer;
 
 public class AfectadorPorPosicion extends Afectador {
