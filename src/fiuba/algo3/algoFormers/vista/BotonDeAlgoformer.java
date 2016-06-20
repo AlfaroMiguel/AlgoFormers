af81a096716b1ae66f1e3642886b6b091192ae57
guidoPeirano <guidopeirano11@hotmail.com>
2016-06-20 02:54:08 -0300

    Tooltip configurado

--- src/fiuba/algo3/algoFormers/vista/BotonDeAlgoformer.java
+++ src/fiuba/algo3/algoFormers/vista/BotonDeAlgoformer.java
@@ -2,6 +2,7 @@ package fiuba.algo3.algoFormers.vista;
 
 import fiuba.algo3.algoFormers.generico.Algoformer;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -29,6 +30,11 @@ public class BotonDeAlgoformer extends Button implements RepresentacionDeAlgofor
 	public void setImagenDeAlgoformer(){
 		Image imagen = ClaseImagenes.getImage(this.algoformer);
 		this.setGraphic(new ImageView(imagen));
+	}
+	public Tooltip configurarTooltip(int vida, int ataque, int velocidad){
+		Tooltip tooltip = new Tooltip();
+		tooltip.setText("Vida: " + this.algoformer.verVida()+ "\nAtaque: "+this.algoformer.verAtaque()+"\nVelocidad: "+this.algoformer.verPaso());
+		return tooltip;
 	}
 	public void setAlgoformer(Algoformer algoformer) {
 		this.algoformer = algoformer;
