86924bb5107fb261ed2c265d560ef422b84cb853
rodrivaldivia <rodrivaldivia10@gmail.com>
2016-06-20 21:15:03 -0300

    doc de controlador

--- src/fiuba/algo3/algoFormers/controlador/ModoSeleccionar.java
+++ src/fiuba/algo3/algoFormers/controlador/ModoSeleccionar.java
@@ -4,7 +4,11 @@ import fiuba.algo3.algoFormers.juego.Juego;
 import fiuba.algo3.algoFormers.tablero.Coordenada;
 
 public class ModoSeleccionar implements ModoControlador {
-
+//	El modo por defecto del controlador.
+//	Se utiliza para seleccionar a 
+//	los algoFormers
+	
+	//	Intenta seleccionar al algoFomer en la coordenada
 	@Override
 	public void fueSeleccionado(Juego juego, Coordenada coordenada) {
 		Boolean seleccionado = juego.puedoSeleccionarCoordenada(coordenada);
--- src/fiuba/algo3/algoFormers/controlador/SilenciadorEventHandler.java
+++ src/fiuba/algo3/algoFormers/controlador/SilenciadorEventHandler.java
@@ -6,6 +6,8 @@ import javafx.scene.input.MouseEvent;
 
 public class SilenciadorEventHandler implements EventHandler<MouseEvent> {
 
+	// Al hacer click sobre el boton de silenciar
+	// se pausara la musica
 	@Override
 	public void handle(MouseEvent event) {
 		Aplicacion.silenciarMusica();
