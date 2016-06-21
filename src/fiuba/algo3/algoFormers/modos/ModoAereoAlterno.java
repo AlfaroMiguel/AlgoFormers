3ba8ac460ae1d3d63cc423dcb5abda8618c11801
rodrivaldivia <rodrivaldivia10@gmail.com>
2016-06-20 22:34:40 -0300

    saco el metodo que no hace nada

--- src/fiuba/algo3/algoFormers/modos/ModoAereoAlterno.java
+++ src/fiuba/algo3/algoFormers/modos/ModoAereoAlterno.java
@@ -6,10 +6,4 @@ import fiuba.algo3.algoFormers.superficie.SuperficieTierra;
 /* Clase que representa al modo aereo en modo alterno */
 public abstract class ModoAereoAlterno extends ModoAereo {
 	
-	/* Metodos abstractos redefinidos */
-	@Override
-	public void reaccionarASuperficie(SuperficieTierra superficie, Algoformer afectado){
-		superficie.afectar(afectado, this);
-	}
-	
 }
